b818d28dd8b043534373b275a9b60f4efd32ec61
Joshua Lee <joshua.lee.99@hotmail.com>
2020-04-03 16:27:44 +0800

    comments

--- blockchain.py
+++ blockchain.py
@@ -6,11 +6,13 @@ import signal
 import hashlib
 from time import sleep
 import pickle
-from random import randint
 import ast
 
 class Block:
     def __init__(self, index, prevhash, data, timestamp):
+        '''
+        Defines Block
+        '''
         self.index = index
         self.prevhash = prevhash
         self.data = data
@@ -18,18 +20,33 @@ class Block:
         self.hash = self.getHash()
 
     def getIndex(self):
+        '''
+        Returns Index of Block
+        '''
         return self.index
     
     def getPrevHash(self):
+        '''
+        Returns Previous Hash of Block
+        '''
         return self.prevhash
 
     def getData(self):
+        '''
+        Returns Data of Block
+        '''
         return self.data
 
     def getTime(self):
+        '''
+        Returns Timestamp
+        '''
         return self.timestamp
 
     def getHash(self):
+        '''
+        Calculates Hash of Block
+        '''
         hashinfo = (str(self.prevhash) + str(self.data) + str(self.timestamp))
         innerhash = hashlib.sha256(hashinfo.encode()).hexdigest().encode()
         outerhash = hashlib.sha256(innerhash).hexdigest()
@@ -37,31 +54,47 @@ class Block:
 
 class Runner:
     def __init__(self):
+        '''
+        Runs the P2P Client
+        '''
         self.peers = []
         self.blockchain = []
-        self.startup()
+        self.startup() # run startup function
         self.udplisten = threading.Thread(target=self.listener, args=("udp",), daemon=True)
-        self.udplisten.start()
+        self.udplisten.start() # run udp listener in thread
         self.tcplisten = threading.Thread(target=self.listener, args=("tcp",), daemon=True)
-        self.tcplisten.start()
+        self.tcplisten.start() # run tcp listener in thread
         self.keepalivet = threading.Thread(target=self.keepalive, daemon=True)
-        self.keepalivet.start()
+        self.keepalivet.start() # run keepalive thread
         print("All processes running.")
         
         kb = threading.Thread(target=self.keyboard)
-        kb.start()
+        kb.start() # run commandline interface thread
 
     def createsocket(self, socktype):
+        '''
+        Function to create a TCP or UDP socket
+        '''
         if socktype == "tcp":
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         elif socktype == "udp":    
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # allow broadcast
+        
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # allow reuse of address
         return sock
 
     # a hacky way to get the interface ip (depends on the number of interfaces on your pc)
-    def getint(self):
+    # only localhost
+    # no firewalls
+    # no weird stuffs
+    # best is VMs
+    def getif(self):
+        '''
+        Gets the interface to bind to the sockets
+        -------------
+        It's a hacky way to get the interface IP (Depends on the # of interfaces on computer)
+        '''
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
         ip = s.getsockname()[0]
@@ -70,22 +103,30 @@ class Runner:
         return ip
 
     def startup(self):
+        '''
+        Starts up the client
+        -------------
+        Sends a UDP broadcast to the network to check if there are peers on the network
+        Waits 5 seconds to receive a response from peers
+        If there are peers, retrieve blockchain and add peers that responded to the peers list
+        '''
         print("Starting Up Blockchain Client...")
         sock = self.createsocket("udp")        
         sock.settimeout(5)
-        sock.bind((self.getint(), 0)) # need to change a
-        sock.sendto(str.encode("teamblock"), ("255.255.255.255", 8080))
+        sock.bind((self.getif(), 0))
+        sock.sendto(str.encode("teamblock"), ("255.255.255.255", 8080)) # send 'teamblock' (keyword to not have false positives) to the broadcast address
         try:
             data, addr = sock.recvfrom(64)
             data = data.decode("utf-8") 
             if data[0:9] == "teamblock":
                 peerslst = ast.literal_eval(data[9:])
                 self.peers = peerslst
-                self.peers.append(addr[0])
+                self.peers.append(addr[0]) # add peer to the list
                 sock.close()    
-        except socket.timeout:
+                
+        except socket.timeout: # wait for timeout
             print("No peers online.")
-            print("Creating Genesis Block...")
+            print("Creating Genesis Block...") # create genesis block since this is the only peer on the network
             sock.close()
             data = {
                 "index": 0,
@@ -97,53 +138,59 @@ class Runner:
             return True
 
     def create(self, data):
+        '''
+        Creates Block and appends to blockchain list
+        '''
         block = Block(data["index"], data["prevhash"], data["data"], data["timestamp"])
         self.blockchain.append(block)
         print("Block Created!")
 
     def listener(self, socktype):
+        '''
+        Runs a listener for either tcp or udp packets
+        '''
         if socktype == "udp":
             while True:
                 ssock = self.createsocket("udp")
-                ssock.bind(("0.0.0.0", 8080))    
-                data, addr = ssock.recvfrom(64)
-                data = data.decode("utf-8") 
-                if data == "teamblock":
+                ssock.bind(("0.0.0.0", 8080))
+                data, addr = ssock.recvfrom(64) # receives udp packets
+                data = data.decode("utf-8")
+                if data == "teamblock": # checks if any client has broadcasted the startup packet
                     print("\nPeer Connected! IP Address:" + str(addr))
-                    ssock.sendto(str.encode("teamblock" + str(self.peers)), addr)
+                    ssock.sendto(str.encode("teamblock" + str(self.peers)), addr) # reply to the client
                     if addr[0] not in self.peers:
-                        self.peers.append(addr[0])
+                        self.peers.append(addr[0]) # appends the ip address of the new client to the peers list
                     sleep(4)
-                    self.sync([addr[0]])
-                elif data == "stillalive?":
-                    ssock.sendto(str.encode("amalive"), addr)
-                elif data == "amleaving":
+                    self.sync([addr[0]]) # send a sync request to the new peer
+                elif data == "stillalive?": # check if the data received is a keepalive packet
+                    ssock.sendto(str.encode("amalive"), addr) # reply to keepalive packet
+                elif data == "amleaving": # checks if the data is an exit packet
                     print("\n" + str(addr[0]) + " has left the chat.")
                     if addr[0] in self.peers:
-                        self.peers.remove(addr[0])
+                        self.peers.remove(addr[0]) # remove peer from list
                 
                 ssock.close()
 
         elif socktype == "tcp":
             while True:
                 tsock = self.createsocket("tcp")
-                tsock.bind((self.getint(), 8080))
+                tsock.bind((self.getif(), 8080))
                 tsock.listen()
                 try:
                     while True:
                         conn, addr = tsock.accept()
-                        fullmsg = b''
-                        data = conn.recv(1024)
+                        fullmsg = b'' # expect bytes 
+                        data = conn.recv(1024) # recv buffer of 1024
                         if data:
-                            msglen = int(data[:10])
+                            msglen = int(data[:10]) # check the message length that is prepended to the data
 
                             fullmsg += data
 
-                            if len(fullmsg)-10 == msglen:
+                            if len(fullmsg)-10 == msglen: # check if full data received
                                 print("\nSync Received from " + str(addr[0]))
                                 if addr[0] not in self.peers:
                                     self.peers.append(addr[0])
-                                data = pickle.loads(fullmsg[10:])
+                                data = pickle.loads(fullmsg[10:]) # decode the pickle data
                                 self.blockchain = data
                 except Exception as e:
                     print("\nError: " + str(e))
@@ -151,15 +198,18 @@ class Runner:
                     tsock.close()
 
     def sync(self, peerslist):
+        '''
+        Sends synchronise request
+        '''
         if len(peerslist) != 0:
-            for ip in peerslist:
+            for ip in peerslist: # loop through each peer in the peers list
                 try:
                     sysock = self.createsocket("tcp")
                     addr = (str(ip), 8080)
                     sysock.connect(addr)
-                    message = pickle.dumps(self.blockchain)
-                    message = bytes(f'{len(message):<10}', "utf-8") + message
-                    sysock.sendall(message)
+                    message = pickle.dumps(self.blockchain) # create pickle object from blockchain
+                    message = bytes(f'{len(message):<10}', "utf-8") + message # prepend len of message to the data
+                    sysock.sendall(message) 
                 except:
                     print("Failed to sync with peer " + str(ip))
                 finally:
@@ -168,24 +218,26 @@ class Runner:
             print("No peers to sync with...")
 
     def keepalive(self):
+        '''
+        Keep Alive function to check if peers have disconnected
+        '''
         while True:
-            sleep(30)
+            sleep(30) # runs every 30 seconds
             for ip in self.peers:
                 x = 0 
-                while x < 3:
+                while x < 3: # 3 attempts before kicking peer
                     try:
                         sock = self.createsocket("udp")
                         sock.settimeout(5)
-                        sock.bind((self.getint(), 0))
-                        # print("\nSending keepalive packet to " + str(ip))
-                        sock.sendto(str.encode("stillalive?"), (ip, 8080))
-                        data, addr = sock.recvfrom(64)
+                        sock.bind((self.getif(), 0))
+                        sock.sendto(str.encode("stillalive?"), (ip, 8080)) # sends stillalive? as data
+                        data, _ = sock.recvfrom(64)
                         data = data.decode("utf-8") 
                         if data == "amalive":
                             x = 3
                             sock.close()
                     except socket.timeout:
-                        if x == 2:
+                        if x == 2: # if the peer has not replied thrice
                             print("\n" + str(ip) + " hasn't been responding for awhile...")
                             print("\nRemoving from active peers.")
                             self.peers.remove(ip)
@@ -196,6 +248,9 @@ class Runner:
                         print("Error: " + str(e))
         
     def keyboard(self):
+        '''
+        Creates Command Line Interface
+        '''
         while True:
             command = input("Enter Command > ")
             if command.lower() == "add" or command.lower() == "create":
@@ -220,7 +275,6 @@ class Runner:
             elif command.lower() == "blocks" or command.lower() == "list" or command.lower() == "ls":
                 print("=========================================")
                 print("Block Info:")
-                # print(self.blockchain)
                 for block in self.blockchain:
                     print("=========================================")
                     print("Index: " + str(block.getIndex()))
@@ -261,7 +315,7 @@ class Runner:
                 else:
                     sock = self.createsocket("udp")        
                     sock.settimeout(5)
-                    sock.bind((self.getint(), 0))
+                    sock.bind((self.getif(), 0))
                     sock.sendto(str.encode("amleaving"), ("255.255.255.255", 8080))
 
                     sys.exit()
@@ -282,9 +336,11 @@ class Runner:
                 print("Command Not Recognised. Enter 'help' for help.")
         
 def exithandler(signal_received, frame):
-    # Handle any cleanup here
+    '''
+    Handles exit from main thread
+    '''
     print('Quitting Program...')
-    exit(0)
+    sys.exit()
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, exithandler)
